a06899b82e51ecabb065371e31123822b4428cf2
Doom <clement.doumergue@epitech.eu>
2017-11-03 13:13:01 +0100

    add multiplatform Viral class
    Change-Id: I007992047cbb6b02878309664d75d7754242bc65

---
+++ Project/Viral/details/WindowsViral.hpp
@@ -0,0 +1,23 @@
+//
+// Created by doom on 03/11/17.
+//
+
+#ifndef SPIDER_CLIENT_WINDOWSVIRAL_HPP
+#define SPIDER_CLIENT_WINDOWSVIRAL_HPP
+
+#ifndef SPIDER_CLIENT_VIRAL_HPP
+#error Do not include this file directly
+#endif
+
+namespace details
+{
+    class ViralImpl
+    {
+    protected:
+        void antiTrace() noexcept
+        {
+        }
+    };
+}
+
+#endif //SPIDER_CLIENT_WINDOWSVIRAL_HPP
